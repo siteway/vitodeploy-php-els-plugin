6650b650ce91888736083c878893e6762f76206f
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2026-04-13 15:20:29 +0600

    added gzip handling and cors handling

--- views/vhost/nginx-php.blade.php
+++ views/vhost/nginx-php.blade.php
@@ -14,6 +14,21 @@ location ~ \.php$ {
     include fastcgi_params;
     fastcgi_hide_header X-Powered-By;
 }
+# Serve TYPO3 pre-compressed .gzip assets with correct MIME types
+location ~* \.css\.gzip$ {
+    add_header Content-Encoding gzip;
+    default_type text/css;
+}
+location ~* \.js\.gzip$ {
+    add_header Content-Encoding gzip;
+    default_type application/javascript;
+}
+
+# CORS for web fonts
+location ~* \.(eot|otf|ttc|ttf|woff2?)$ {
+    add_header Access-Control-Allow-Origin "*";
+}
+
 index index.php index.html;
 error_page 404 /index.php;
 #[/php]
